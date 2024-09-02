65507fecd1c2df5c7ae62087d9d89c51c5a23253
FaZil-shaik <ammia4770@gmail.com>
2024-09-02 11:05:24 +0530

    "made changes"

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("", {
+			const socket = io("https://mernchat-rn5e.onrender.com/", {
 				query: {
 					userId: authUser._id,
 				},
